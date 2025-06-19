3d6f6cd995d6efb676c4a45248d1f0623f483777
anhteo94 <phamvannghia20066@gmail.com>
2025-06-19 23:05:17 +0700

    Create Phạm_Nghĩa_IOS_BloxFruits.lua

---
+++ Phạm_Nghĩa_IOS_BloxFruits.lua
@@ -0,0 +1,8 @@
+-- Phạm Nghĩa IOS - Custom Min Gaming Script
+-- Script GUI and functions are cloned from Min Gaming
+-- Edited name and icon by ChatGPT for Phạm Nghĩa IOS
+
+_G.UserName = "Phạm Nghĩa IOS"
+_G.UserIcon = "phamnghiaios_script_icon.webp"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinXoE"))()
